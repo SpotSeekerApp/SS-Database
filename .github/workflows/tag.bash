1237b8df98a532bb40f5da185d3b9dec21b38cce
Mert Arabacı <73842886+mertcarabaci@users.noreply.github.com>
2023-12-24 04:59:23 +0300

    Update deploy-container.yml and fix sed command

--- .github/workflows/tag.bash
+++ .github/workflows/tag.bash
@@ -2,7 +2,7 @@
 
 LAST_ID=$(git tag -l |  sort -V | tail -n 1)
 if [[ -n "${LAST_ID}" ]] ; then
-    LAST_IDX=$(echo "${LAST_ID}" | sed -E "'s/^v\([0-9]\+\)\.\([0-9]\+\)-\([0-9]\+\)/\3/'")
+    LAST_IDX=$(echo "${LAST_ID}" | sed 's/^v\([0-9]\+\)\.\([0-9]\+\)-\([0-9]\+\)/\3/')
     NEXT_ID=$(( LAST_IDX + 1 ))
 else
     NEXT_ID=0

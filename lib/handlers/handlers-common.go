e8932545c1dc7f3f97de6eaa7bf9fb2ecc2964ae
Mert Arabaci <arabaci19@itu.edu.tr>
2024-01-04 22:58:12 +0300

    API Key to secure requests
    Signed-off-by: Mert Arabaci <arabaci19@itu.edu.tr>

--- lib/handlers/handlers-common.go
+++ lib/handlers/handlers-common.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"SS-Database/utils"
 	"cloud.google.com/go/firestore"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -17,7 +19,24 @@ type HandlerInstance struct {
 const userReqTypes = "user|feedback|favorite|password|filter|review"
 const placeReqTypes = "place|tag"
 
+func (h HandlerInstance) VerifyAPICall(w http.ResponseWriter, r *http.Request) bool {
+
+	fileData := make(map[string]string, 0)
+	jsonFile, _ := os.ReadFile("api-key.json")
+	json.Unmarshal(jsonFile, &fileData)
+	return r.Header.Get("API-Key") == fileData["api-key"]
+}
+
 func (h HandlerInstance) HandleRequest(w http.ResponseWriter, r *http.Request) {
+	if h.VerifyAPICall(w, r) != true {
+		w.Header().Set("Content-Type", "application/json")
+
+		jsonResp, _ := json.Marshal(map[string]http.ConnState{
+			"StatusCode": http.StatusUnauthorized,
+		})
+		_, _ = w.Write(jsonResp)
+		return
+	}
 	fmt.Println(r.URL.Path)
 	reqPath := strings.ToLower(r.URL.Path)
 
